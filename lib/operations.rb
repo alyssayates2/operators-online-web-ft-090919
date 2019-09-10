c40625a5af03cb63cb8bf79f95d48c2082abf53c
Alyssa Yates <bosslys@icloud.com>
2019-09-10 20:02:16 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,11 +2,11 @@ def unsafe?(speed)
   if (speed > 60 || speed < 40)
     return TRUE
   else
-    return FALSE  
+    return FALSE
 end
 
 
 
 def not_safe?(speed)
-
+speed > 60 || speed < 40 ? return TRUE : return FALSE 
 end
